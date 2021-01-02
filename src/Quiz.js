5f4f292b5863b2ad43c217fb5e62df6a495b41c2
Abhishek Galoda <Abhishek.Galoda@currenciesdirect.com>
2021-01-02 23:46:05 +0000

    Added a Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,14 +14,13 @@ class Quiz extends Component{
     }
 
     render(){
-        const isQuizEnd = quizData.quiz_questions.length == this.state.quiz_position-1
+        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position-1
         return  (
         <div>
             <div className="QuizQuestion">
-               <QuizQuestion quiz_question = { quizData.quiz_questions[this.state.quiz_position-1] } />
-                
-               isQuizEnd ? <QuizEnd />:""
-
+                { isQuizEnd ? 
+                <QuizEnd />:
+                <QuizQuestion quiz_question = { quizData.quiz_questions[this.state.quiz_position-1] } />  }
                
             </div>
         </div>
